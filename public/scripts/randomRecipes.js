7f1584e0c9a7d726992f322049661a7a6c4e2101
Spencer Marx <spencer.s.marx@gmail.com>
2019-03-04 14:50:44 -0300

    clear randomRecipes.js

--- public/scripts/randomRecipes.js
+++ public/scripts/randomRecipes.js
@@ -3,18 +3,11 @@ const Recipe = require("../../models/recipe");
 const Day = require("../../models/day");
 
 function randomNumber() {
-    Recipe.find({}, function(err, foundRecipes) {
-        if (err) {
-            console.log(err);
-        } else {
-            var num = Math.round(Math.random() * foundRecipes.length + 1);
-            console.log(num)
-        }
-    });
+
 }
 
 function randomRecipes() {
-    return randomNumber();
+
 }
 
 
