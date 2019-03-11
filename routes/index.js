63f3c0ad1c3e9d9a635d6f0ffbd0ed54fb9fca7a
Spencer Marx <spencer.s.marx@gmail.com>
2019-03-11 18:22:27 -0300

    Add register and login routes, add register and login pages with basic semantic forms

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,62 @@
 var express = require("express");
 var router = express.Router();
-// var passport = require("passport");
-// var User = require("../models/user");
+var passport = require("passport");
+var User = require("../models/user");
 // =================
 // INDEX ROUTING
 // =================
 
 // LANDING - Index Route
-router.get("/", (req, res) => {
+router.get("/", isLoggedIn, (req, res) => {
     res.redirect("/dashboard");
 });
 
+// Signup
+router.get("/register", function(req, res) {
+    res.render("register");
+});
+
+// Handle Signup Logic
+router.post("/register", function(req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/dashboard");
+            });
+        }
+    });
+});
+
+// Login
+router.get("/login", function(req, res) {
+    res.render("login");
+})
+
+// Handle Login
+router.post("/login", passport.authenticate("local", {
+        successRedirect: "/dashboard",
+        failureRedirect: "/login"
+    }),
+    function(req, res) {}
+);
+
+// Logout
+router.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+})
+
+
+// Middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
