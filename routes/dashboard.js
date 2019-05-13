80afac0ca77849321b9fa703418cd50a19c16d64
Spencer Marx <spencer.s.marx@gmail.com>
2019-05-13 10:22:23 -0300

    Initial markup and styling for info bar, still in need of text styling and EJS data

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -11,18 +11,21 @@ var randomRecipes = require("../public/scripts/randomRecipes");
 // =================
 
 // INDEX
-router.get("/", middleware.isLoggedIn, middleware.checkPlan, (req, res) => {
-    User.findOne({
-            username: req.user.username
-        })
-        .populate("plan.lunch")
-        .populate("plan.dinner")
-        .exec(function(err, user) {
-            // console.log("Dashboard Load ->", user.plan);
-            res.render("dashboard", {
-                plan: user.plan
-            });
-        });
+router.get("/", middleware.isLoggedIn, (req, res) => {
+    res.render("dashboard");
+        //             plan: user.plan
+        //         });
+    // User.findOne({
+    //         username: req.user.username
+    //     })
+    //     .populate("plan.lunch")
+    //     .populate("plan.dinner")
+    //     .exec(function(err, user) {
+    //         // console.log("Dashboard Load ->", user.plan);
+    //         res.render("dashboard", {
+    //             plan: user.plan
+    //         });
+    //     });
 });
 
 
